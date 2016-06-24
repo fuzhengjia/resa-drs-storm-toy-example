992e12e412285077c7ab1749985dd1391b85aadb
fuzhengjia <fuzhengjia@gmail.com>
2016-06-24 13:53:32 +0800

    Initial commit

---
+++ src/main/java/IntervalSupplier.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+
+/**
+ * Created by ding on 14-4-15.
+ */
+public interface IntervalSupplier extends Serializable {
+
+    long get();
+
+}
---
+++ src/main/java/TAWordCounter.java
@@ -0,0 +1,28 @@
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+/**
+ * Created by ding on 14-1-27.
+ */
+public class TAWordCounter extends TASleepBolt {
+
+    public TAWordCounter(IntervalSupplier sleep) {
+        super(sleep);
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        super.execute(tuple);
+        String word = tuple.getString(1);
+        collector.emit(new Values(word + "!!"));
+        collector.ack(tuple);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        ///declarer.declare(new Fields("word", "count"));
+        declarer.declare(new Fields("word!"));
+    }
+}
